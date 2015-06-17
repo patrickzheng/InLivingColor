d39563665eb7731e2488424213b6dfb369b185b4
rhymeswithlion <rhymeswithlion@users.noreply.github.com>
2015-06-17 11:04:41 -0700

    update

--- server/ingest_photos-start_service.sh
+++ server/ingest_photos-start_service.sh
@@ -1,3 +1,3 @@
 
 
-kafka-console-consumer --zookeeper localhost:2181 --consumer.config ~/kafkatest/consumerconfig.txt --topic test-downloadbyphotoid | python copy_by_json_to_cassandra.py
+kafka-console-consumer --zookeeper localhost:2181 --consumer.config consumerconfig.txt --topic test-downloadbyphotoid | python copy_by_json_to_cassandra.py
